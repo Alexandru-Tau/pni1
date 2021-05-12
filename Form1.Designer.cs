c583b91f69a7a227112b98bc0087d105f2f71029
Alexandru <alextauteanu99@gmail.com>
2021-05-12 18:51:29 +0300

    ss

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Proiect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_incarca = new System.Windows.Forms.Button();
+            this.btn_salveaza = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_hist = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 140);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_incarca
+            // 
+            this.btn_incarca.Location = new System.Drawing.Point(2, 13);
+            this.btn_incarca.Name = "btn_incarca";
+            this.btn_incarca.Size = new System.Drawing.Size(180, 23);
+            this.btn_incarca.TabIndex = 1;
+            this.btn_incarca.Text = "Incarca imagine";
+            this.btn_incarca.UseVisualStyleBackColor = true;
+            this.btn_incarca.Click += new System.EventHandler(this.btn_incarca_Click);
+            // 
+            // btn_salveaza
+            // 
+            this.btn_salveaza.Location = new System.Drawing.Point(189, 13);
+            this.btn_salveaza.Name = "btn_salveaza";
+            this.btn_salveaza.Size = new System.Drawing.Size(133, 23);
+            this.btn_salveaza.TabIndex = 2;
+            this.btn_salveaza.Text = "Salveaza";
+            this.btn_salveaza.UseVisualStyleBackColor = true;
+            this.btn_salveaza.Click += new System.EventHandler(this.btn_salveaza_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btn_hist
+            // 
+            this.btn_hist.Location = new System.Drawing.Point(340, 12);
+            this.btn_hist.Name = "btn_hist";
+            this.btn_hist.Size = new System.Drawing.Size(118, 23);
+            this.btn_hist.TabIndex = 6;
+            this.btn_hist.Text = "Histagram";
+            this.btn_hist.UseVisualStyleBackColor = true;
+            this.btn_hist.Click += new System.EventHandler(this.btn_hist_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_hist);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_salveaza);
+            this.Controls.Add(this.btn_incarca);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_incarca;
+        private System.Windows.Forms.Button btn_salveaza;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_hist;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.UI;
+using System.Drawing.Imaging;
+
+namespace Proiect
+{
+    public partial class Form1 : Form
+    {
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_incarca_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image File(*.jpg;*.jpeg;*.bmp;)|*.jpg;*jpeg;*.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = open.FileName;
+                pictureBox1.Image = Image.FromFile(open.FileName);
+                
+            }
+        }
+
+
+        private void btn_salveaza_Click(object sender, EventArgs e)
+        {
+            File.Copy(textBox1.Text, Path.Combine(@"D:\pni\Proiect\Save Images", Path.GetFileName(textBox1.Text)), true);
+            label2.Text = "Image saved";
+
+        }
+
+        private void btn_hist_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (pictureBox1.Image == null)
+                {
+                    return;
+                }
+
+                var img = new Bitmap(pictureBox1.Image)
+                       .ToImage<Gray, byte>();
+
+                Mat hist = new Mat();
+                float[] ranges = new float[] { 0, 255 };
+                int[] channel = { 0 };
+                int[] histSize = { 256 };
+
+                VectorOfMat ms = new VectorOfMat();
+                ms.Push(img);
+
+                CvInvoke.CalcHist(ms, channel, null, hist, histSize, ranges, false);
+
+                HistogramViewer viewer = new HistogramViewer();
+                viewer.Text = "Image Histogram";
+                viewer.ShowIcon = false;
+                viewer.HistogramCtrl.GenerateHistogram("Image Histogram", Color.Blue, hist,
+                    histSize[0], ranges);
+                viewer.HistogramCtrl.Refresh();
+                viewer.Show();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
